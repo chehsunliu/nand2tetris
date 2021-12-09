fe3eb6d31777abcf171d00bc034efd5565f47d8f
chehsunliu <chehsunliu@gmail.com>
2021-12-10 01:18:36 +0800

    Complete Project 4

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,54 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+(INPUT_CHECK)
+  @KBD
+  D=M
+  @CHOOSE_WHITE_COLOR
+  D;JEQ
+  @CHOOSE_BLACK_COLOR
+  0;JMP
+
+(CHOOSE_WHITE_COLOR)
+  @color
+  M=0
+  @DRAW_INIT
+  0;JMP
+
+(CHOOSE_BLACK_COLOR)
+  @color
+  M=-1
+  @DRAW_INIT
+  0;JMP
+
+(DRAW_INIT)
+  @8191
+  D=A
+  @offset
+  M=D
+
+(DRAW_CHECK_OFFSET)
+  @offset
+  D=M
+  @INPUT_CHECK
+  D;JLT
+
+(DRAW_START)
+  @SCREEN
+  D=A
+  @offset
+  D=D+M
+  @address
+  M=D
+  @color
+  D=M
+  @address
+  A=M
+  M=D
+
+(DRAW_MINUS)
+  @offset
+  M=M-1
+  @DRAW_CHECK_OFFSET
+  0;JMP
