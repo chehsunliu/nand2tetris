9a95efcc2f0be790f5dbf6d2340cf00df7d97b36
chehsunliu <chehsunliu@gmail.com>
2021-12-23 03:38:15 +0800

    Add cli written in go

---
+++ nand2tetris-go/go.mod
@@ -0,0 +1,3 @@
+module github.com/chehsunliu/nand2tetris/nand2tetris-go
+
+go 1.17
---
+++ nand2tetris-go/wrapper/VMTranslator.py
@@ -0,0 +1,7 @@
+# https://github.com/chehsunliu/nand2tetris
+import subprocess
+import sys
+
+
+if __name__ == "__main__":
+    subprocess.call(["./nand2tetris-linux.exe"])
