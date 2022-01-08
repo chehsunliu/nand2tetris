47df28d79f3140ab71e122e156246e7cfc7209d9
chehsunliu <chehsunliu@gmail.com>
2022-01-09 01:02:41 +0800

    Remove unused

--- nand2tetris-go/main.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "github.com/chehsunliu/nand2tetris/nand2tetris-go/cmd"
-
-func main() {
-	cmd.Execute()
-}
--- nand2tetris-go/wrapper/VMTranslator.py
+++
@@ -1,7 +0,0 @@
-import subprocess
-import sys
-
-
-# The source code is on GitHub: https://github.com/chehsunliu/nand2tetris.
-if __name__ == "__main__":
-    subprocess.call(["./nand2tetris-linux.exe", "vm-translator", "--input-path", sys.argv[1]])
